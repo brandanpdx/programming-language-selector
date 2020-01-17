9b73b50a4f240b3d2cf64e23d5cb0bfd742ce1fe
Brandan Sayarath <bsayarath@gmail.com>
2020-01-17 14:59:08 -0800

    add javascript if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,29 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var nameResult = $("input#name").val();
+    var subject = $("#subject").val();
+    var coast = $("#coast").val();
+    var dessert = $("#dessert").val();
+    var cereal = $("#cereal").val();
+
     console.log(nameResult)
 
+    if (nameResult === "undefined") {
+      alert("Please enter your name!");
+    } else if (subject === "english") {
+      $(".nameresult").text(nameResult);
+      $("#results").show();
+      $("#C").hide();
+      $("#Haskell").hide();
+    } else if (subject === "math") {
+      $(".nameresult").text(nameResult);
+      $("#results").show();
+      $("#Python").hide();
+      $("#Haskell").hide();
+    } else {
+      $(".nameresult").text(nameResult);
+      $("#results").show();
+    }
 
-
-    $(".nameresult").text(nameResult);
-  })
-
+    })
 })
